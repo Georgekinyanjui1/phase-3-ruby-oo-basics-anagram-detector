83dddd06b78bc953df815931949b9415799b34b6
georgekinyanjui1 <george.kinyanjui@student.moringaschool.com>
2022-08-31 13:50:06 +0300

    commit tree 4edf163224f5174bccf052e11e3cba3c8f36555d

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+    def initialize(word)
+      @word = word
+    end
+  
+    def match(array)
+      found_array = []
+      array.each do |word|
+        if word.chars.sort.join == @word.chars.sort.join
+          found_array << word
+        end
+      end
+      return found_array
+    end
+  end
